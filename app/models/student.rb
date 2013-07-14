198bffde3681748a563d3145037479b0a69efb52
Abraham Sangha <abraham.sangha@gmail.com>
2013-07-13 22:44:48 -0500

    validated email, phone

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,33 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  validates :email, format: { with: /.@.+\.../ }, uniqueness: true
+  validates :birthday, presence: true, if: :old_enough?
+  validates :phone, format: { with: /\d{3}.*\d{3}.*\d{4}/ }
+
+  def name
+  	"#{self.first_name} #{self.last_name}"
+  end
+
+  def age
+  	now = Date.today
+  	@age = now.year - self.birthday.year - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
+  end
+  def old_enough?
+  	@age.to_i > 4
+  end
 end
+
+# def name_with_party
+#     "#{self.firstname} #{self.lastname} (#{self.party})"
+#   end
+
+
+
+# Add validations to the Student model so that a student cannot be saved unless
+# the following requirements are met:
+
+# Email addresses must contain at least one @ character and one . character,
+# with at least one character before the @, one character between the @ and
+# first ., and at least two characters after the final .. Email addresses
+# must be unique across all students. Students must be at least 5 years old.
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -4,6 +4,27 @@ require_relative '../config'
 
 class CreateStudents < ActiveRecord::Migration
   def change
+  	create_table :students do |t|
+  		t.string :first_name
+  		t.string :last_name
+  		t.string :gender
+  		t.date   :birthday
+  		t.string :email
+  		t.string :phone
+  	end
     # HINT: checkout ActiveRecord::Migration.create_table
   end
 end
+
+
+# ------------+
+# | students   |
+# +------------+
+# | id         |
+# | first_name |
+# | last_name  |
+# | gender     |
+# | birthday   |
+# | email      |
+# | phone      |
+# +------------+
